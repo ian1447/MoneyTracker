796de6ca98e775bee538289ff3aca99e19f515c5
Ric Ian Jamora <48129671+ian1447@users.noreply.github.com>
2025-11-03 14:20:34 +0800

    Name

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Money Tracker</title>
+      <meta name="description" content="Track your money, banks, and e-wallets" />
+    </>
+  );
+}
